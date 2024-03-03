82370e2d786c3bb428129537d5353c1374518de7
jingyu.you <101238231+38revolver@users.noreply.github.com>
2024-03-03 20:04:17 +0900

    Update sample2.py

--- dags/sample2.py
+++ dags/sample2.py
@@ -4,22 +4,23 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'airflow_yjg',
     'depends_on_past': False,
     'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1)
 }
 
 dag = DAG(
-    'start_and_stop', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'dumm_test', default_args=default_args, schedule_interval=timedelta(minutes=1))
 
 
 start = DummyOperator(task_id='start', dag=dag)
+mid = DummyOperator(task_id='mid', dag=dag)
 
 end = DummyOperator(task_id='end', dag=dag)
 
-start >> end
+start >> mid >> end
